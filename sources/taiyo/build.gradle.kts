0f272d3b75cdf0d1a7ed32a24741ba33277fac24
gabrieldesignhd-rgb <gabrieldesignhd-rgb@users.noreply.github.com>
2026-07-01 01:30:05 -0300

    Add Taiyō source

---
+++ sources/taiyo/build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    alias(kei.plugins.extension)
+}
+
+keiyoushi {
+    name = "Taiyō"
+    versionCode = 1
+    contentWarning = ContentWarning.SAFE
+    libVersion = "1.4"
+
+    source {
+        lang = "pt-BR"
+        baseUrl = "https://taiyo.moe"
+    }
+}
---
+++ sources/taiyo/src/eu/kanade/tachiyomi/extension/pt/taiyo/Taiyo.kt
@@ -0,0 +1,19 @@
+package eu.kanade.tachiyomi.extension.pt.taiyo
+
+import eu.kanade.tachiyomi.source.online.ParsedHttpSource
+import keiyoushi.annotation.Source
+
+/**
+ * Esqueleto gerado pelo Mihon Source Studio.
+ *
+ * Este site não foi reconhecido como Madara. Complete os requests e parsers
+ * usando os seletores reais do site antes de compilar a extensão.
+ */
+@Source
+abstract class Taiyo : ParsedHttpSource() {
+    // TODO: implementar popularMangaRequest/popularMangaSelector/popularMangaFromElement
+    // TODO: implementar searchMangaRequest/searchMangaSelector/searchMangaFromElement
+    // TODO: implementar mangaDetailsParse
+    // TODO: implementar chapterListSelector/chapterFromElement
+    // TODO: implementar pageListParse
+}
