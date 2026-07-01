72ef78e76c9a430e9069da56eb428fe1d1b966ec
gabrieldesignhd-rgb <gabrieldesignhd-rgb@users.noreply.github.com>
2026-07-01 01:38:06 -0300

    Fix Taiyo skeleton: extend HttpSource instead of ParsedHttpSource
    ParsedHttpSource doesn't exist in this extensions-lib version, which
    broke compilation for the whole repo (all modules build together).

--- sources/taiyo/src/eu/kanade/tachiyomi/extension/pt/taiyo/Taiyo.kt
+++ sources/taiyo/src/eu/kanade/tachiyomi/extension/pt/taiyo/Taiyo.kt
@@ -1,19 +1,21 @@
 package eu.kanade.tachiyomi.extension.pt.taiyo
 
-import eu.kanade.tachiyomi.source.online.ParsedHttpSource
+import eu.kanade.tachiyomi.source.online.HttpSource
 import keiyoushi.annotation.Source
 
 /**
  * Esqueleto gerado pelo Mihon Source Studio.
  *
  * Este site não foi reconhecido como Madara. Complete os requests e parsers
- * usando os seletores reais do site antes de compilar a extensão.
+ * usando os seletores/API reais do site antes de compilar a extensão.
+ * IMPORTANTE: use HttpSource (não ParsedHttpSource, que não existe nesta API).
  */
 @Source
-abstract class Taiyo : ParsedHttpSource() {
-    // TODO: implementar popularMangaRequest/popularMangaSelector/popularMangaFromElement
-    // TODO: implementar searchMangaRequest/searchMangaSelector/searchMangaFromElement
+abstract class Taiyo : HttpSource() {
+    // TODO: implementar popularMangaRequest/popularMangaParse
+    // TODO: implementar searchMangaRequest/searchMangaParse
+    // TODO: implementar latestUpdatesRequest/latestUpdatesParse
     // TODO: implementar mangaDetailsParse
-    // TODO: implementar chapterListSelector/chapterFromElement
+    // TODO: implementar chapterListParse (ou fetchChapterList)
     // TODO: implementar pageListParse
 }
